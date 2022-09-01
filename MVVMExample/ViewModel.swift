46d71d7cd8423fc1297bc2da75e1c3574dee6344
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-09-01 13:35:00 +0200

    scene will connect to viewController setup

--- MVVMExample/ViewModel.swift
+++ MVVMExample/ViewModel.swift
@@ -9,6 +9,10 @@ import Foundation
 
 extension ViewController {
     final class ViewModel {
+        private var persistantStorage: Persistansable!
+        init(persistantStorage: Persistansable) {
+            self.persistantStorage = persistantStorage
+        }
         func start() {
             
         }
